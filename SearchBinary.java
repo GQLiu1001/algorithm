7af6b8452850e359a1d5f117ed56c6a274a30d77
RabbitTank <moondust1196551872@gmail.com>
2026-01-15 10:44:17 +0800

    update

--- SearchBinary.java
+++ SearchBinary.java
@@ -1,13 +1,14 @@
 public class SearchBinary {
     public static void main(String[] args) {
         // 有序数组 查找 target -> 二分
-        int[] arr = {3,5,56,57,89,99,101,333,939,1993,20003};
+        int[] arr = { 3, 5, 56, 57, 89, 99, 101, 333, 939, 1993, 20003 };
         var target = 99;
-        var index = binarySearch(arr,target);
+        var index = binarySearch0(arr, target);
         System.out.println(index);
     }
 
-     static int binarySearch(int[] arr, int target) {
+    static int binarySearch(int[] arr, int target) {
+        // 左闭右闭 左右均参与运算
         int left = 0;
         int right = arr.length - 1;
         while (left <= right) {
@@ -15,13 +16,32 @@ public class SearchBinary {
             int mid = (left + right) >>> 1;
             if (arr[mid] == target) {
                 return mid;
-            }else if(arr[mid] > target){
+            } else if (arr[mid] > target) {
                 right = mid - 1;
                 continue;
-            }else{
+            } else {
                 left = mid + 1;
             }
         }
         return -1;
-     }
+    }
+
+    static int binarySearch0(int[] arr, int target) {
+        // 左闭右开 右不参与运算 
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            // int mid = (left + right) / 2;
+            int mid = (left + right) >>> 1;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                right = mid; // 右不参与运算 
+                continue;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
